d3b334d70643c9ee517bfad17f04f498874f4f81
corselia <takiya@toran.sakura.ne.jp>
2019-12-23 13:54:10 +0900

    「コンポーネントによる構成」の項を終了

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello, Vue.js!'
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
   }
 })
