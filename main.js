37e79e665462bdb7bbde22e9c5b25502cf14bd17
corselia <takiya@toran.sakura.ne.jp>
2019-12-23 13:44:46 +0900

    v-model を用いた双方向バインディングの項を終了

--- main.js
+++ main.js
@@ -2,10 +2,5 @@ const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello, Vue.js!'
-  },
-  methods: {
-    reverseMessage: function() {
-      this.message = this.message.split('').reverse().join('')
-    }
   }
 })
