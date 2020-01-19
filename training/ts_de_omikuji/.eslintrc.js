1d9d27b81991228ff084303e51ec2e7e82a78695
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-19 17:45:51 +0900

    TypeScript 向けに ESLint の設定ファイルを編集した

---
+++ training/ts_de_omikuji/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+};
