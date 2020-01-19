baa1937435e7ed7ecc1085c72ad7897c2ca834ed
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-19 20:52:27 +0900

    ルートディレクトリにある ESLint の設定ファイルにも Jest のグローバル設定を書き、プラグイン設定も書いた（now.sh で落ちたので）

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', 'jest'],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
+  plugins: ['jest'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
