1d86de84d6a78fa08ceb272c09efa562a570a6c7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-19 17:50:46 +0900

    TypeScript のコンパイルが通ることを確認した

--- training/ts_de_omikuji/.eslintrc.js
+++ training/ts_de_omikuji/.eslintrc.js
@@ -8,4 +8,10 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    // 厳し目なので、必要に応じて調整する
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
 };
