cbaa627188950952aaa44efd96552fa933cf9e75
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-19 18:01:02 +0900

    Jest を追加した

--- training/ts_de_omikuji/.eslintrc.js
+++ training/ts_de_omikuji/.eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    'jest/globals': true,
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   rules: {
     // 厳し目なので、必要に応じて調整する
     '@typescript-eslint/indent': 'off',
