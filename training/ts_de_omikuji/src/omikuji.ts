3204fe62791ae4faae4eccbfd2baa433044435aa
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-19 17:45:30 +0900

    TypeScript を書くことを前提のディレクトリ構成にした

---
+++ training/ts_de_omikuji/src/omikuji.ts
@@ -0,0 +1,16 @@
+// class Omikuji {
+//   constructor(array) {
+//     this.items = array;
+//   }
+
+//   get() {
+//     const rand = Math.floor(Math.random() * this.items.length);
+//     return this.items[rand];
+//   }
+// }
+
+// module.exports = Omikuji;
+
+export default function(str1: string, str2: string) {
+  return str1 + str2;
+}
