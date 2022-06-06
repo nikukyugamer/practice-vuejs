7bfd1f116f0e1dd7da1b94631250b11e3283728f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-07 00:49:37 +0900

    test: 💍 axios のロジックをリファクタした

--- tests/unit/views/AboutThisSite.spec.ts
+++ tests/unit/views/AboutThisSite.spec.ts
@@ -1,38 +1,49 @@
+import axios from "axios";
 import { shallowMount } from "@vue/test-utils";
 import AboutThisSite from "@/views/AboutThisSite.vue";
 
-jest.mock("axios", () => ({
-  get: jest.fn(() =>
-    // Promise.reject({})
-    Promise.resolve({
-      data: [
-        {
-          id: 1,
-          name: "Leanne Graham",
-          username: "Bret",
-          email: "Sincere@april.biz",
-          address: {
-            street: "Kulas Light",
-            suite: "Apt. 556",
-            city: "Gwenborough",
-            zipcode: "92998-3874",
-            geo: {
-              lat: "-37.3159",
-              lng: "81.1496",
-            },
-          },
-          phone: "1-770-736-8031 x56442",
-          website: "hildegard.org",
-          company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets",
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const successUserFetchMockedAxios = () => {
+  mockedAxios.get.mockResolvedValue({
+    data: [
+      {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+          zipcode: "92998-3874",
+          geo: {
+            lat: "-37.3159",
+            lng: "81.1496",
           },
         },
-      ],
-    })
-  ),
-}));
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+          name: "Romaguera-Crona",
+          catchPhrase: "Multi-layered client-server neural-net",
+          bs: "harness real-time e-markets",
+        },
+      },
+    ],
+  });
+};
+
+const failUserFetchMockedAxios = () => {
+  mockedAxios.get.mockRejectedValue({
+    data: "ERROR: Fetch User data",
+  });
+};
+
+beforeEach(() => {
+  successUserFetchMockedAxios();
+});
 
 describe("AboutThisSite.vue", () => {
   it("mounted() で axios の結果が期待どおりであること", (done) => {
@@ -44,6 +55,7 @@ describe("AboutThisSite.vue", () => {
     // $nextTick だとエラーになる
     // https://zenn.dev/fuku710/articles/2b0b66a3283f7e
     setTimeout(() => {
+      // expect(mockedAxios.get).toHaveBeenCalled();
       expect(wrapper.text()).toMatch("Leanne Graham");
       expect(wrapper.text()).toMatch("Sincere@april.biz");
       expect(wrapper.text()).not.toMatch("データの受信に失敗しました。");
@@ -78,6 +90,19 @@ describe("AboutThisSite.vue", () => {
 
     wrapper.vm.$nextTick(() => {
       expect(wrapper.text()).toMatch("Hello, AboutThisSiteWorld!");
+      done();
+    });
+  });
+
+  it("ユーザーデータのフェッチに失敗したときに期待通りの動作をすること", (done) => {
+    failUserFetchMockedAxios();
+    const wrapper = shallowMount(AboutThisSite);
+
+    setTimeout(() => {
+      expect(wrapper.text()).toMatch("This is an about page");
+      expect(wrapper.text()).not.toMatch("Leanne Graham");
+      expect(wrapper.text()).toMatch("データの受信に失敗しました。");
+
       done();
     });
   });
